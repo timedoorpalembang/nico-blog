beadc4536facd6ac83aea53f10793aa8691c9761
hamidahwrii <139754394+hamidahwrii@users.noreply.github.com>
2025-08-02 10:40:57 +0700

    modal

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 function changeText() {
-    var replaceText = document.
-    getElementByClassName("mini-text");
-    replaceText[0].innerHTML = "scroll untuk melihat";
-
+    var replaceText = document.getElementsByClassName("mini-text");
+    replaceText[0].innerHTML = "Scroll untuk melihat";
+ 
     document.getElementId("icon-up").style.display = "none";
-    <button id="icon-up" onclick="changeText()"> <i class="far fa-hand-point-up"></i></button>
+ 
     
 
     document.getElementId("icon-down").style.display= "block";
 }
 
+
+
 var btnSubmit = document.querySelector('.submit');
 var modal = document.querySelector('.modal-container');
 
 if (btnSubmit) {
-   btnSubmit.addEventListener('click', function () {
+  btnSubmit.addEventListener('click', function () {
     modal.classList.add('show');
 
     const next = document.querySelector('.next');
-    next.setAttribute('value','https://cobee-the-junior-programmer.netlify.app/contact.html');
+    next.setAttribute('value', 'https://cobee-the-junior-programmer.netlify.app/contact.html');
   });
 }
         
+
